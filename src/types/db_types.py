e8012b17a0329868fd9f16ae1d02712687746594
Rowan Lavelle <rowanlavelle@RowansMacStudio.lan>
2024-02-10 12:27:55 -0500

    fixed primary keys

--- src/types/db_types.py
+++ src/types/db_types.py
@@ -1,23 +1,25 @@
 TEAMS_SCHEMA = """
 DROP TABLE IF EXISTS teams;
 CREATE TABLE IF NOT EXISTS teams (
-    team_id INTEGER PRIMARY KEY,
+    team_id INTEGER,
     team_name TEXT,
-    team_slug TEXT
+    team_slug TEXT,
+    PRIMARY KEY (team_id)
 );"""
 
 PLAYERS_SCHEMA = """
 DROP TABLE IF EXISTS players;
 CREATE TABLE IF NOT EXISTS players (
-    player_id INTEGER PRIMARY KEY,
+    player_id INTEGER,
     player_name TEXT,
-    player_slug TEXT
+    player_slug TEXT,
+    PRIMARY KEY (player_id)
 );"""
 
 GAMES_SCHEMA = """
 DROP TABLE IF EXISTS games;
 CREATE TABLE IF NOT EXISTS games (
-    game_id INTEGER PRIMARY KEY,
+    game_id TEXT,
     season TEXT,
     season_type TEXT,
     season_type_code TEXT,
@@ -26,7 +28,8 @@ CREATE TABLE IF NOT EXISTS games (
     home_team INTEGER,
     away_team INTEGER,
     home_score INTEGER,
-    away_score INTEGER
+    away_score INTEGER,
+    PRIMARY KEY (game_id)
 );"""
 
 STATS_SCHEMA = """
@@ -34,7 +37,7 @@ DROP TABLE IF EXISTS player_stats;
 CREATE TABLE IF NOT EXISTS player_stats (
     player_id INTEGER,
     team_id INTEGER,
-    game_id INTEGER,
+    game_id TEXT,
     position TEXT,
     minutes FLOAT,
     fieldGoalsMade INTEGER,
@@ -55,6 +58,6 @@ CREATE TABLE IF NOT EXISTS player_stats (
     turnovers INTEGER,
     foulsPersonal INTEGER,
     points INTEGER,
-    plusMinusPoints FLOAT,
+    plusMinusPoints FLOAT, 
     PRIMARY KEY (game_id, player_id)
 );"""
