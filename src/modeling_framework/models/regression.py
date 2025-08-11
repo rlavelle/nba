25e1c57d9f4b69eb817dd886ec526e538a6af884
rlavelle <rowan.lavelle@gmail.com>
2025-08-11 22:49:09 +0800

    base for logistic reg

--- src/modeling_framework/models/regression.py
+++ src/modeling_framework/models/regression.py
@@ -1,10 +1,21 @@
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, LogisticRegression
 from src.modeling_framework.framework.model import Model
 
 
 class LinearModel(Model):
     def build_model(self, **params):
         self.model = LinearRegression()
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def fit(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+
+class LogitModel(Model):
+    def build_model(self, **params):
+        self.model = LogisticRegression()
 
     def predict(self, X):
         return self.model.predict(X)
