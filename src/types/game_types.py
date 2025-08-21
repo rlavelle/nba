bc841c99e7b215fbd157217d6eb6f02f97c0668b
rlavelle <rowan.lavelle@gmail.com>
2025-08-21 19:41:39 +0800

    game features

--- src/types/game_types.py
+++ src/types/game_types.py
@@ -1,9 +1,40 @@
 from enum import Enum
 from typing import Literal
 
+GAME_FEATURES = [
+    'minutes', 'pointsOffTurnovers',
+    'pointsSecondChance', 'pointsFastBreak', 'pointsPaint',
+    'oppPointsOffTurnovers', 'oppPointsSecondChance', 'oppPointsFastBreak',
+    'oppPointsPaint', 'fieldGoalsMade', 'fieldGoalsAttempted',
+    'fieldGoalsPercentage', 'threePointersMade', 'threePointersAttempted',
+    'threePointersPercentage', 'freeThrowsMade', 'freeThrowsAttempted',
+    'freeThrowsPercentage', 'reboundsOffensive', 'reboundsDefensive',
+    'reboundsTotal', 'assists', 'steals', 'blocks', 'blocksAgainst',
+    'turnovers', 'foulsPersonal', 'foulsDrawn', 'points', 'plusMinusPoints',
+    'effectiveFieldGoalPercentage', 'trueShootingPercentage',
+    'usagePercentage', 'estimatedUsagePercentage', 'offensiveRating',
+    'estimatedOffensiveRating', 'defensiveRating',
+    'estimatedDefensiveRating', 'netRating', 'estimatedNetRating',
+    'assistPercentage', 'assistToTurnover', 'assistRatio', 'turnoverRatio',
+    'teamTurnoverPercentage', 'offensiveReboundPercentage',
+    'defensiveReboundPercentage', 'reboundPercentage',
+    'freeThrowAttemptRate', 'pace', 'estimatedPace', 'pacePer40',
+    'possessions', 'PIE', 'oppEffectiveFieldGoalPercentage',
+    'oppFreeThrowAttemptRate', 'oppTeamTurnoverPercentage',
+    'oppOffensiveReboundPercentage', 'percentageFieldGoalsAttempted2pt',
+    'percentageFieldGoalsAttempted3pt', 'percentagePoints2pt',
+    'percentagePointsMidrange2pt', 'percentagePoints3pt',
+    'percentagePointsFastBreak', 'percentagePointsFreeThrow',
+    'percentagePointsOffTurnovers', 'percentagePointsPaint',
+    'percentageAssisted2pt', 'percentageUnassisted2pt',
+    'percentageAssisted3pt', 'percentageUnassisted3pt',
+    'percentageAssistedFGM', 'percentageUnassistedFGM'
+]
 
-SeasonTypeLiteral = Literal["Regular Season", "Playoffs", "All-Star", "Preseason", "Summer League", "PlayIn", "IST Championship"]
+SeasonTypeLiteral = Literal[
+    "Regular Season", "Playoffs", "All-Star", "Preseason", "Summer League", "PlayIn", "IST Championship"]
 StatTypeLiteral = Literal["starters", "bench", "statistics"]
+
 
 class StatType(str, Enum):
     STARTERS = "starters"
