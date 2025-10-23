82e2238404af315b4e49566ae5c2fbc57d812871
rlavelle <rowan.lavelle@gmail.com>
2025-10-23 18:11:50 +0700

    stndardized feature building from scripts

---
+++ src/feature_engineering/utils/build_features.py
@@ -0,0 +1,187 @@
+import pickle
+import time
+
+import pandas as pd
+
+from src.feature_engineering.base import FeaturePipeline
+from src.feature_engineering.bayes_posterior import BayesPosteriorFeature
+from src.feature_engineering.last_game_value import LastGameValueFeature
+from src.feature_engineering.moving_avg import ExponentialMovingAvgFeature, CumSeasonAvgFeature, CumSeasonEMAFeature, \
+    SimpleMovingAvgFeature
+from src.feature_engineering.player_streak import PlayerHotStreakFeature
+from src.feature_engineering.utils.cache import gen_cache_file, check_cache
+from src.logging.logger import Logger
+from src.modeling_framework.framework.dataloader import NBADataLoader
+from src.types.game_types import GAME_FEATURES, CURRENT_SEASON
+from src.types.player_types import PlayerType, PLAYER_FEATURES
+
+
+def get_ft_cols(df):
+    wanted_subs = ["_bayes_post", "_1g", "_sma_", "_ema_", "_cum_ssn_", "_hot_streak"]
+
+    cols = [
+        col for col in df.columns
+        if any(sub in col for sub in wanted_subs)
+    ]
+
+    return cols
+
+def build_ft_sets(df, features, id, ):
+    for f in features:
+        features = [
+            ExponentialMovingAvgFeature(span=7, source_col=f, group_col=(id,)),
+            ExponentialMovingAvgFeature(span=5, source_col=f, group_col=(id,)),
+            ExponentialMovingAvgFeature(span=3, source_col=f, group_col=(id,)),
+            CumSeasonAvgFeature(source_col=f, group_col=(id, 'season')),
+            CumSeasonEMAFeature(source_col=f, group_col=(id, 'season')),
+            SimpleMovingAvgFeature(window=3, source_col=f, group_col=(id,)),
+            SimpleMovingAvgFeature(window=5, source_col=f, group_col=(id,)),
+            LastGameValueFeature(source_col=f, group_col=(id,))
+        ]
+
+        dependents = []
+        for feature in features:
+            if '_cum_ssn' in feature.feature_name:
+                dependents.append(
+                    BayesPosteriorFeature(ybar_col=feature.feature_name,
+                                          source_col=f,
+                                          id_col=id,
+                                          group_col=(id, 'season'))
+                )
+
+            if '3g' not in feature.feature_name and '1g' not in feature.feature_name:
+                dependents.append(
+                    PlayerHotStreakFeature(window=1,
+                                           comp_col=feature.feature_name,
+                                           source_col=f,
+                                           group_col=(id, 'season'))
+                )
+
+                dependents.append(
+                    PlayerHotStreakFeature(window=3,
+                                           comp_col=feature.feature_name,
+                                           source_col=f,
+                                           group_col=(id, 'season'))
+                )
+
+            if '3g' in feature.feature_name:
+                dependents.append(
+                    PlayerHotStreakFeature(window=1,
+                                           comp_col=feature.feature_name,
+                                           source_col=f,
+                                           group_col=(id, 'season'))
+                )
+
+        features.extend(dependents)
+        pipeline = FeaturePipeline(features)
+        return pipeline.transform(df, sort_order=(id, 'season', 'date'))
+
+
+def build_game_lvl_fts(logger: Logger = None, cache: bool = False):
+    start = time.time()
+
+    if logger:
+        logger.log(f'[STARTING GAME FEATURE BUILDING]')
+
+    if cache:
+        if logger:
+            ret = check_cache(f='game_fts', logger=logger)
+        else:
+            ret = check_cache(f='game_fts')
+
+        if ret is not None:
+            return ret
+
+    data_loader = NBADataLoader()
+    data_loader.load_data()
+
+    if logger:
+        t = time.time()
+        logger.log(f'[DATA LOADED]: {round((t - start), 2)}s')
+
+    games = data_loader.get_data('games')
+    games = games.dropna()
+    games = games.sort_values(by=['team_id', 'season', 'date'])
+
+    # for predictions, for each team we add a null row
+    #   - row contains the team id, curr season, and a date of 2030-01-01
+    teams = games[['team_id']].drop_duplicates()
+    future_rows = pd.DataFrame(columns=games.columns)
+
+    future_rows['team_id'] = teams['team_id']
+    future_rows['season'] = CURRENT_SEASON
+    future_rows['date'] = pd.Timestamp('2030-01-01')
+
+    games = pd.concat([games, future_rows], ignore_index=True)
+    games = build_ft_sets(games, GAME_FEATURES, 'team_id')
+
+    fts_cols = get_ft_cols(games)
+    games = games.dropna(subset=fts_cols)
+    games = games[games['season'] > games['season'].min()].copy()
+
+    if logger:
+        end = time.time()
+        logger.log(f'[FEATURE BUILDING COMPLETE]: {round((end - start), 2)/60}s')
+
+    try:
+        with open(gen_cache_file('game_fts'), "wb") as f:
+            pickle.dump(games, f)
+    except Exception as e:
+        if logger:
+            logger.log(f'[ERROR ON SAVING CACHE]: {e}')
+
+    return games
+
+
+def build_player_lvl_fts(logger: Logger = None, cache: bool = False):
+    start = time.time()
+
+    if logger:
+        logger.log(f'[STARTING PLAYER FEATURE BUILDING]')
+
+    if cache:
+        if logger:
+            ret = check_cache(f='player_fts', logger=logger)
+        else:
+            ret = check_cache(f='player_fts')
+
+        if ret is not None:
+            return ret
+
+    data_loader = NBADataLoader()
+    data_loader.load_data()
+
+    if logger:
+        t = time.time()
+        logger.log(f'[DATA LOADED]: {round((t - start), 2)}s')
+
+    player_data = data_loader.get_player_type(ptypes=(PlayerType.STARTER,))
+    player_data = player_data[~player_data.spread.isna()].copy()
+    player_data = player_data.drop(columns=['position'])
+    player_data = player_data.dropna()
+    player_data = player_data.sort_values(by=['player_id', 'season', 'date'])
+
+    # for predictions, for each player we add a null row
+    #   - row contains the player id, curr season, and a date of 2030-01-01
+    players = player_data[['player_id']].drop_duplicates()
+    future_rows = pd.DataFrame(columns=player_data.columns)
+
+    future_rows['player_id'] = players['player_id']
+    future_rows['season'] = CURRENT_SEASON
+    future_rows['date'] = pd.Timestamp('2030-01-01')
+
+    player_data = pd.concat([player_data, future_rows], ignore_index=True)
+    player_data = build_ft_sets(player_data, PLAYER_FEATURES, 'player_id')
+
+    if logger:
+        end = time.time()
+        logger.log(f'[FEATURE BUILDING COMPLETE]: {round((end - start)/60, 2)}s')
+
+    try:
+        with open(gen_cache_file('player_fts'), "wb") as f:
+            pickle.dump(player_data, f)
+    except Exception as e:
+        if logger:
+            logger.log(f'[ERROR ON SAVING CACHE]: {e}')
+
+    return player_data
---
+++ src/feature_engineering/utils/cache.py
@@ -0,0 +1,33 @@
+import configparser
+import datetime
+import os
+import pickle
+
+from src.config import CONFIG_PATH
+from src.db.utils import insert_error
+from src.utils.date import date_to_dint
+
+
+def get_cache_dir():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_PATH)
+    return config.get('DATA_PATH', 'cache_folder')
+
+
+def gen_cache_file(f):
+    today = date_to_dint(datetime.date.today())
+    return os.path.join(get_cache_dir(), f'{today}_{f}.pkl')
+
+
+def check_cache(f, logger=None):
+    cache_file = gen_cache_file(f)
+
+    if os.path.exists(cache_file):
+        try:
+            with open(cache_file, "rb") as file:
+                logger.log(f'[SUCCESS ON CACHE HIT]: {cache_file}')
+                return pickle.load(file)
+        except Exception as e:
+            logger.log(f'[ERROR LOADING CACHE]: {e}')
+            insert_error({'msg': str(e)})
+            return None
