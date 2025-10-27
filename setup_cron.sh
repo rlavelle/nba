6ed9b7c45316522e00c31077a769e8336bd4829c
rlavelle <rowan.lavelle@gmail.com>
2025-10-27 13:43:44 +0545

    auto pulling

--- setup_cron.sh
+++ setup_cron.sh
@@ -9,12 +9,14 @@ CRON_JOB_2="5 5 * * * cd $PROJECT_DIR && python3 -m src.scrapers.jobs.get_next_n
 
 CRON_JOB_3="0 7 * * * cd $PROJECT_DIR && python3 -m src.modeling_framework.jobs.build_models"
 CRON_JOB_4="0 10 * * * cd $PROJECT_DIR && python3 -m src.modeling_framework.jobs.predict"
+CRON_JOB_5="59 23 * * * cd $PROJECT_DIR && git pull"
 
 # Install cron jobs (prevent duplicates)
 ( crontab -l | grep -v "get_prev_nba_data"; echo "$CRON_JOB_1" ) | crontab -
 ( crontab -l | grep -v "get_next_nba_data"; echo "$CRON_JOB_2" ) | crontab -
 ( crontab -l | grep -v "build_models"; echo "$CRON_JOB_3" ) | crontab -
 ( crontab -l | grep -v "predict"; echo "$CRON_JOB_4" ) | crontab -
+( crontab -l | grep -v "git pull"; echo "$CRON_JOB_5" ) | crontab -
 
 echo "Cron jobs installed"
 crontab -l
