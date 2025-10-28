da56c55643c70682212d235f0cc2b47ff77752c8
rlavelle <rowan.lavelle@gmail.com>
2025-10-28 15:04:34 +0545

    quick manual script for full pipeline

--- scrape.sh
+++ scrape.sh
@@ -1,4 +1,4 @@
-python3 -m src.scrapers.jobs.get_prev_nba_data
+python3 -m src.scrapers.jobs.get_prev_nba_data --date="2025-10-27"
 python3 -m src.scrapers.jobs.get_next_nba_data
 python3 -m src.modeling_framework.jobs.build_models
 python3 -m src.modeling_framework.jobs.predict
