579fc3b925b0481de3db2ace4648026907c2da0c
rlavelle <rowan.lavelle@gmail.com>
2025-12-19 10:35:02 -0500

    script to fill in missed blocks of back data

---
+++ src/scrapers/nba/scripts/scrape_range.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import argparse
+from datetime import datetime
+import subprocess
+
+from src.utils.date import generate_dates
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start-date", type=str, required=True, help="YYYY-MM-DD")
+    parser.add_argument("--end-date", type=str, required=True, help="YYYY-MM-DD")
+    args = parser.parse_args()
+
+    start = datetime.strptime(args.start_date, "%Y-%m-%d").date()
+    end = datetime.strptime(args.end_date, "%Y-%m-%d").date()
+
+    for date in generate_dates(start, end):
+        diso = date.isoformat()
+        print(f"Running for {diso}")
+        subprocess.run([
+            "python3",
+            "-m",
+            "src.scrapers.jobs.get_prev_nba_data",
+            f"--date={diso}",
+        ], check=True)
