1e26f62c8be6ae613eab28d83ae73c0353045abb
rlavelle <rowan.lavelle@gmail.com>
2025-12-20 23:53:45 -0500

    fixing cron issues

--- scrape.sh
+++ scrape.sh
@@ -1,4 +1,5 @@
-python3 -m src.scrapers.jobs.get_prev_nba_data --date="2025-10-27"
-python3 -m src.scrapers.jobs.get_next_nba_data
-python3 -m src.modeling_framework.jobs.build_models
-python3 -m src.modeling_framework.jobs.predict
+#!/bin/bash
+venv/bin/python3 -m src.scrapers.jobs.get_prev_nba_data --date=$(date -v -1d +%Y-%m-%d) && \
+venv/bin/python3 -m src.scrapers.jobs.get_next_nba_data && \
+venv/bin/python3 -m src.modeling_framework.jobs.build_models && \
+venv/bin/python3 -m src.modeling_framework.jobs.predict
