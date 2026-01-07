fc2caeccb018bd2009814d5596ec8f4673934282
rlavelle <rowan.lavelle@gmail.com>
2026-01-07 13:06:50 -0500

    cleanup for prev day cache

--- scrape.sh
+++ scrape.sh
@@ -2,5 +2,5 @@
 venv/bin/python3 -m src.scrapers.jobs.get_prev_nba_data --date=$(date -v -1d +%Y-%m-%d) && \
 venv/bin/python3 -m src.scrapers.jobs.get_next_nba_data && \
 venv/bin/python3 -m src.modeling_framework.jobs.build_models && \
-venv/bin/python3 -m src.modeling_framework.jobs.predict && \
+venv/bin/python3 -m src.modeling_framework.jobs.predict --clean-up && \
 venv/bin/python3 -m src.modeling_framework.jobs.grade_predictions --admin
